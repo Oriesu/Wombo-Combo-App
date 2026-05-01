1cffdb9d574cee474c0ed08299683fd851215c47
Oriesu <javiersanchezmartinez2006@gmail.com>
2026-05-01 16:30:20 +0200

    Remove android folder - iOS only project

--- android/app/src/main/kotlin/com/example/wombo_combo_nuevo_definitivo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.wombo_combo_nuevo_definitivo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/javiersanchez/wombo_combo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.javiersanchez.wombo_combo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
